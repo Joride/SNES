749d49c9c6d7cb03130340f2c59063057206f119
Joride <kerrelinc@gmail.com>
2015-09-13 21:26:50 +0200

    Initial commit

---
+++ SNES/SNESKit.h
@@ -0,0 +1,20 @@
+//
+//  SNESKit.h
+//  SNES
+//
+//  Created by Joride on 13-09-15.
+//  Copyright © 2015 Kerrelinc. All rights reserved.
+//
+
+//! Project version number for SNES.
+FOUNDATION_EXPORT double SNESVersionNumber;
+
+//! Project version string for SNES.
+FOUNDATION_EXPORT const unsigned char SNESVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNES/PublicHeader.h>
+
+#import <SNES/SNESConstants.h>
+#import <SNES/SNES.h>
+#import <SNES/SNESVideoOutputLayer.h>
+#import <SNES/CGGameControllerForSNESInterpreter.h>
